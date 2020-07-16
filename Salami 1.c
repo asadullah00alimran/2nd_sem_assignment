4352a9a27db272c9ccc276df265b2457f584b043
Asadullah Al Imran <66588286+asadullah00alimran@users.noreply.github.com>
2020-07-16 22:11:24 +0600

    Add files via upload

---
+++ Salami 1.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main()
+
+{
+    printf("I\n");
+    printf("  want\n");
+    printf("     to be a\n");
+    printf("          good\n");
+    printf("             programmer");
+
+
+    return 0;
+
+}
