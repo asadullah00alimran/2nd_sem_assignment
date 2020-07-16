d13f12fef16f0e73f15255770fe20fa0932c438d
Asadullah Al Imran <66588286+asadullah00alimran@users.noreply.github.com>
2020-07-16 22:12:58 +0600

    Add files via upload

---
+++ Salami 2.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+int main()
+
+{
+    int number1,number2,sum;
+
+    printf("Input first number: ");
+    scanf("%d",&number1);
+
+    printf("Input second number: ");
+    scanf("%d",&number2);
+
+    sum = number1 + number2;
+
+    printf("Sum = %d",sum);
+
+    return 0;
+}
