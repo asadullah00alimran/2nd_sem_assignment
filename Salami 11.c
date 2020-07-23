e0c3e4567ddf467a5a8b87ddefed57aefb7e6b02
Asadullah Al Imran <66588286+asadullah00alimran@users.noreply.github.com>
2020-07-22 22:17:20 -0700

    Add files via upload

---
+++ Salami 11.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+int main()
+
+{
+    int number;
+
+    printf("Enter a number :");
+    scanf("%d",&number);
+
+    if(number >= 0)
+        printf("\nInput is positive ");
+
+    else
+        printf("\nInput is negative");
+
+        printf("\n\n");
+
+        return 0;
+
+}
