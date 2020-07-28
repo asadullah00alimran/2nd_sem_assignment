7991fb37b66ee95925a0c02fbcb930e77148c989
Asadullah Al Imran <66588286+asadullah00alimran@users.noreply.github.com>
2020-07-27 17:15:12 -1200

    Add files via upload

---
+++ salami 18.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+  int main()
+{
+  int number;
+
+  printf("Enter any number to find the absolute value for : ");
+  scanf("%d",&number);
+  if(number<0)
+{
+  number=(-1)*number;
+  printf("Absolute value is =%d",number);
+}
+
+{if(number>0)
+
+    printf("\nAbsolute value is =%d",number);
+
+}
+  return (0);
+}
