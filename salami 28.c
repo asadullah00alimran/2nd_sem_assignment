ea6a2902b9f1a2b4be507c85200aa11ea2e66a4a
Asadullah Al Imran <66588286+asadullah00alimran@users.noreply.github.com>
2020-10-04 04:10:43 -1200

    Add files via upload

---
+++ salami 28.c
@@ -0,0 +1,21 @@
+
+#include<stdio.h>
+int main () {
+
+int i, ulimit,llimit;
+
+printf( "Input Upper limit : ");
+scanf("%d",&ulimit);
+
+printf( "Input Lower limit : ");
+scanf("%d",&llimit);
+
+for (i=ulimit; i<=llimit; i++)
+
+
+        printf("%d,", i);
+
+
+ return 0;
+    }
+
