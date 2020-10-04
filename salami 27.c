cd26e678f364f54e76ed2c84c0e56ee152bc9bdf
Asadullah Al Imran <66588286+asadullah00alimran@users.noreply.github.com>
2020-10-04 04:09:50 -1200

    Add files via upload

---
+++ salami 27.c
@@ -0,0 +1,20 @@
+
+#include <stdio.h>
+
+int main()
+{
+    int i, N;
+
+    printf("Enter value: ");
+    scanf("%d", &N);
+
+
+    for
+        (i=1; i<=N; i++)
+        printf("%d, ", i);
+    {
+        printf(" %d ", i);
+    }
+
+    return 0;
+}
